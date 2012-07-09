15b755bc5219fdc8ac30d6ba0057745dafa2de4b
A Flores <ctrl4ltdeleteme@gmail.com>
2012-07-09 12:28:35 +0200

    adding a function to append the container and adding some helper methods to find the base location etc

--- travelavenue-trip-widget.js
+++ travelavenue-trip-widget.js
@@ -3,7 +3,18 @@
     ww: window,       // DOM window
     dd: document,     // DOM document
     nn: navigator,    // Browser navigator object
-    pp: properties,   // hash of properties
+    pp: properties,   // Hash of properties
+    // Finds the this script tag from within the page
+    // It is called here as a function to insure it will only be called once
+    taScript: function(){
+      var objs    = document.getElementsByTagName('script'), 
+          i       = 0;
+      for(; i < objs.length; i++){ 
+        if(objs[i].src.search('travelavenue-trip-widget') > 0){
+          return objs[i];
+        }
+      }
+    }(),
     // make: build an HTML element given a hash structure
     //  example: div_element = _ta.make({div: {id: 'id'}});
     make: function(obj) {
@@ -11,7 +22,7 @@
 
       for (elem in obj) {
         if (obj[elem].hasOwnProperty) {
-          createdObject = tt.dd.createElement(elem);
+          createdObject = _ta.dd.createElement(elem);
           // assign the attributes
           for (attr in obj[elem]) {
             if (obj[elem][attr].hasOwnProperty) {
@@ -26,17 +37,21 @@
       return createdObject;
     },
     init: function(){
-      var buttonContainer,
+      var buttonContainer;
       // Build button
       buttonContainer = _ta.make({
         div: {
           className: _ta.pp.gid + '_button_container'
         }
       });
-      // Add button to page
+      
+      // Add button container to page
+      _ta.taScript.parentNode.insertBefore(buttonContainer, _ta.pp.taScript);
       // Add events to button
     }
   }
+
+  _ta.init();
 })(window, document, navigator, {
   gid: "TA_TRIP_WIDGET_" + (new Date).getTime(),
   msg:{
